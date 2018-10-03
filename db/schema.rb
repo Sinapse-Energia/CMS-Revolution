a0ef6c415fe04b2456c1cd7e296885c1524bf3e2
enbake <kshtzsinha>
2018-10-03 13:34:30 +0530

    database.yml chnages

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_01_070517) do
+ActiveRecord::Schema.define(version: 2018_10_01_082620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "static_data", force: :cascade do |t|
+    t.text "name", null: false
+    t.text "id_code", null: false
+    t.text "id_communication", null: false
+    t.text "location", null: false
+    t.decimal "longitude", precision: 8, scale: 2, null: false
+    t.decimal "latitude", precision: 8, scale: 2, null: false
+    t.decimal "altitude", precision: 8, scale: 2, null: false
+    t.datetime "date_installation", null: false
+    t.integer "circuit_number", null: false
+    t.text "name_street"
+    t.text "number_street"
+    t.text "power_installed"
+    t.text "power_contracted"
+    t.text "id_supply_contract"
+    t.text "clock_brand"
+    t.text "clock_model"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
