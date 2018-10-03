6cebac028b57670d734f31188dcb16d726a5f0da
Anjana <anjana.enbake@gmail.com>
2018-10-03 18:57:05 +0530

    UI is added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
-  root 'session#create'
+  root 'session#index'
   resources :session do
+  	get 'index', to: 'session#index', on: :collection
+  	get 'register', to: 'session#register', on: :collection
      get 'forget_password', to: 'session#forget_password', on: :collection
      post 'reset_password', to: 'session#reset_password', on: :collection
      get 'change_password_view', to: 'session#change_password_view', on: :collection
