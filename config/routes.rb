39b861089ab51f27ee90da4ae0c73842cf9f90b3
Anjana <anjana.enbake@gmail.com>
2018-10-06 12:33:36 +0530

    flash messages added

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'session#index'
   resources :session do
-  	# get 'index', to: 'session#index', on: :collection
+  	get 'index', to: 'session#index', on: :collection
   	post 'create', to: 'session#create_session', on: :collection
   	get 'register', to: 'session#register', on: :collection
   	post 'create_register', to: 'session#create_register', on: :collection
