b378d3d573c08f02e0c87119dbbf718ba6688cf4
Anjana <anjana.enbake@gmail.com>
2018-10-10 13:41:59 +0530

    edit action performed

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -15,7 +15,6 @@ class DevicesController < ApplicationController
   def create
      @device = Device.new(device_params)
     if @device.save
-      # @device.create_mqtt_broker
       redirect_to :devices
     else
       redirect_to new_device_path
@@ -27,23 +26,18 @@ class DevicesController < ApplicationController
   end
 
   def update
-      if params[:device][:api_json].present?
-      # For device management when upload json
-      file = params[:device][:api_json].read
-      data = JSON.parse(file)
-      @device.update(api_json: data, template_id: params[:device][:template_id])
-    else
-      # Normal device updation
-      @device.update(device_params)
-    end
-    redirect_to new_device_path
+    @device.update(device_params)
+    redirect_to devices_path
   end
-
-  # MQTT device connect operations
-  
+ 
  private
 
   def device_params
     params.require(:device).permit(:name, :id_code, :id_communication, :location, :longitude, :latitude, :altitude, :date_installation, :circuit_number, :name_street, :number_street, :power_installed, :power_contracted, :id_supply_contract, :clock_brand, :clock_model)
   end
-end
+
+  def fetch_device
+    @device = Device.find_by(id: params[:id])
+  end
+
+  end
