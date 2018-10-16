b1dbc4cda0b2b9969e8f96d103a19572048f83b9
kshitiz <ksinha@enbake.com>
2018-10-16 13:18:45 +0530

    deploy path changes

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@
 set :application, 'CMS-Revolution'
 set :repo_url,        'git@github.com:Sinapse-Energia/CMS-Revolution.git'
 ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-set :deploy_to, '/var/www/CMS-Revolution'
+set :deploy_to, '/var/www/'
 set :pty, true
 set :linked_files, %w{config/database.yml config/application.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
