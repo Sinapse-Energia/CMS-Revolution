b6a5546d9536a0bf7de62845d399dbb3fdf8a761
kshitiz <ksinha@enbake.com>
2018-10-17 13:44:09 +0530

    precomile is added in production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,7 +75,7 @@ Rails.application.configure do
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
-  Rails.application.config.assets.precompile += %w( *.js ^[^_]*.css *.css.erb )
+  config.assets.precompile += %w( *.js ^[^_]*.css *.css.erb )
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
