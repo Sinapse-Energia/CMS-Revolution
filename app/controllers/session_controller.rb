dd25cb7c6d9ca32760763bcb481f5c539907be98
kshitiz <ksinha@enbake.com>
2018-11-12 12:16:44 +0530

    reset session controller

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -48,29 +48,28 @@ end
   end
   
   def reset_password
-      @user = User.find_by(email: params[:email])
-      if @user.present?
-        @url  = "http://localhost:3000/session/change_password_view?id=#{@user.id}"
-        UserMailer.reset_password_email(@user, @url).deliver_now
-        flash[:notice] = " An email has been sent to reset your password "
-        redirect_to root_path
-      else
-        flash[:notice] = "Email does not Exists. Please provide valid email"
-       end
-
+    @user = User.find_by(email: params[:email])
+    if @user.present?
+      @url  = "http://localhost:3000/session/change_password_view?id=#{@user.id}"
+      UserMailer.reset_password_email(@user, @url).deliver_now
+      flash[:notice] = " An email has been sent to reset your password "
+      redirect_to root_path
+    else
+      flash[:notice] = "Email does not Exists. Please provide valid email"
+     end
   end
 
   def change_password_view
   end
 
   def change_password
-       @user = User.find_by(id: params[:id])
-       if @user
-          @user.update_attributes!(password: params[:password])
-          redirect_to root_path
-       else
-         flash[:notice] = "No user exists"
-       end
+    @user = User.find_by(id: params[:id])
+    if @user
+      @user.update_attributes!(password: params[:password])
+      redirect_to root_path
+    else
+      flash[:notice] = "No user exists"
+    end
   end
 
   def logout
